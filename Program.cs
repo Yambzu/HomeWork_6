1b0064f78b6d97c4e68959de782c424d2de8b3f4
Ann <loshadkkaann@mail.ru>
2022-11-02 21:04:34 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+/*
+int[] CreateRandomArray() 
+{
+    Console.Write("Input a number of elements: ");
+    int size = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a min possible value: ");
+    int min = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a max possible value: ");
+    int max = Convert.ToInt32(Console.ReadLine());
+
+  int[] array = new int[size];
+  
+  for (int i = 0; i < array.Length; i++)
+  {
+    array[i] = new Random().Next(min, max + 1);
+  }
+
+  return array;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+int PositiveNumber(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+            count++;
+    }    
+    return count;
+}
+ 
+int[] myArray = CreateRandomArray();
+ShowArray(myArray);
+int posNum = PositiveNumber(myArray);
+Console.WriteLine($"Positive numbers > 0: {posNum}");
+*/
+
+//Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+/*
+Console.Write("Введите k1: ");
+var k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b1: ");
+var b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+var k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+var b2 = Convert.ToDouble(Console.ReadLine());
+ 
+ 
+var x = -(b1 - b2) / (k1 - k2);
+var y = k1 * x + b1;
+ 
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
+ 
+Console.WriteLine($"Пересечение в точке: ({x};{y})");
+*/
